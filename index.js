bafff25c0608a1d254cd23614dd69c17d7908692
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-04 12:53:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+var num1 = 2
+var num2 = 31
+
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = 16 % 6;
+
+var max = Math.max(2, 1, 8, 16, 20, 11)
